1ebe5f4a6956f17cffdb602afdade3a586b859fa
shamil <muhammedshamilml10@gmail.com>
2023-03-15 09:36:57 +0530

    ss

---
+++ flipkart/index.js
@@ -0,0 +1,39 @@
+let categories = [{
+    name: "Electronics",
+    img: ""
+    },
+    {
+        name: "TVs & Appliances",
+        img: ""
+    },
+    {
+        name: "Men",
+        img: ""
+    },
+    {
+        name: "Women",
+        img: ""
+    },
+    {
+        name: "Baby & Kids",
+        img: ""
+    },
+    {
+        name: "Home & Furniture",
+        img: ""
+    },
+    {
+        name: "Sports,Book & More",
+        img: ""
+    },
+    {
+        name: "Flights",
+        img: ""
+    },
+    {
+        name: "offer Zone",
+        img: ""
+    }
+
+    
+]
