b2bdbd8b20f1a96a6375dc26dd941ad521051c25
shamil <muhammedshamilml10@gmail.com>
2023-03-16 09:30:27 +0530

    feat:nav

--- flipkart/index.js
+++ flipkart/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let categories = [{
     name: "Electronics",
     img: ""
@@ -37,3 +38,19 @@ let categories = [{
 
     
 ]
+=======
+let num = prompt("enter a number");
+
+sum = 0;
+
+function sumAll() {
+    for (let i = 0; i < num; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+console.log(sumAll(sum))
+>>>>>>> afeced3 (feat:nav)
