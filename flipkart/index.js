afeced31ab07400718c27cf649b802d1bac6fc21
shamil <muhammedshamilml10@gmail.com>
2023-03-16 09:30:27 +0530

    feat:nav

---
+++ flipkart/index.js
@@ -0,0 +1,14 @@
+let num = prompt("enter a number");
+
+sum = 0;
+
+function sumAll() {
+    for (let i = 0; i < num; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+console.log(sumAll(sum))
